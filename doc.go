10bc6518a51256bbcd069e215473c68eca64c0bc
bogcon <bog.con.bc@gmail.com>
2022-05-24 18:04:08 +0300

    fix linter

--- doc.go
+++ doc.go
@@ -3,6 +3,6 @@
 // license that can be found in the LICENSE file or at
 // https://github.com/actforgood/xdi/LICENSE.
 
-// Package `xdi` provides a centralized dependency injection manager which holds
+// Package xdi provides a centralized dependency injection manager which holds
 // definitions for an application's objects/dependencies.
 package xdi
